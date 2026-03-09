4bc556969770e5f5909a95ff1aef953ff33317a8
Alba Mora de la Sen <jalbamora@gmail.com>
2026-03-09 17:35:06 +0100

    fix vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 export default defineConfig({
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/portfolio-albamora/",
+  base: "/",
 })
