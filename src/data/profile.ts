a65915a0a9b2199500d1ecaa580d8fda7c6b818c
Alba Mora de la Sen <jalbamora@gmail.com>
2026-03-09 14:19:19 +0100

    Fix Download CV button

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -16,8 +16,8 @@ export const profile: Profile = {
     name: "Alba Mora de la Sen",
     headline: "Desarrolladora Fullstack",
     summary: "Soy una persona curiosa e inquieta que está siempre en busca de nuevos conocimientos, es por eso, que he encontrado en el mundo del desarrollo web un hueco donde poder desarrollar todo mi potencial y poder contribuir en proyectos reales.",
-    photoUrl: "public/profile-photo.png",
-    cvUrl: "public/cv-alba-mora-de-la-sen.pdf",
+    photoUrl: `${import.meta.env.BASE_URL}profile-photo.png`,
+    cvUrl: `${import.meta.env.BASE_URL}cv-alba-mora-de-la-sen.pdf`,
     links: {
         github: "https://github.com/albamdls",
         linkedin: "https://www.linkedin.com/in/alba-mora-de-la-sen/"
